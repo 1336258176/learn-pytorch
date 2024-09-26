40800af10b069b1a614c3837fe310245b94dfcc3
LyBin <lybin1336258176@outlook.com>
2024-09-26 20:40:22 +0800

    perf(gitignore): add gitignore

---
+++ main.py
@@ -0,0 +1,8 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+if __name__ == '__main__':
+    print('hello world')
